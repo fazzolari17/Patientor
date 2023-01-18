f587036c397396828965256fce5d5f452df63d1c
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-01-18 11:30:59 -0500

    add health check route

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,11 @@ app.use(express.json());
 
 app.get('/api/ping', (_req, res) => {
   console.log(`Someone Pinged Here`);
-  res.send('pong');
+  res.status(200).send('pong');
+});
+
+app.get('/health', (_req, res) => {
+  res.status(200).send('200 ok');
 });
 
 app.use('/api/diagnoses', diagnosesRouter);
