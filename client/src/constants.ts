5fbaef901435a121e82edd28c791cf9d3ae0ccd4
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-01-19 16:23:00 -0500

    fix build path

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,8 +1,8 @@
-// const PORT = process.env.port;
+// const PORT = process.env.port || 10000;
 
 const production = `https://patientor-ccvj.onrender.com`;
-// const local = 'http://localhost:3001';
+const local = 'http://localhost:3001';
 
-// const url = process.env.NODE_ENV === 'development' ? local : production;
-
+const url = process.env.NODE_ENV === 'production' ? production : local;
+console.log(process.env.NODE_ENV)
 export const apiBaseUrl = `${production}/api`;
