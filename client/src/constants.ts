d5640e750052db323b554cd71b9776c0a159bdcc
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-01-19 15:47:14 -0500

    update api url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,1 +1,6 @@
-export const apiBaseUrl = "http://localhost:3001/api";
+const production = 'https://patientor-ccvj.onrender.com';
+const local = 'http://localhost:3001';
+
+const url = process.env.NODE_ENV === 'development' ? local : production;
+
+export const apiBaseUrl = `${url}/api`;
