bf4169923471ac240094712e656f023d6b00a16c
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-03-11 08:24:30 -0500

    initial commit

---
+++ client/src/services/interceptors.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { logout } from '../reducers/userReducer';
+import store from '../store';
+
+const UNAUTHORIZED = 401;
+const { dispatch } = store; // direct access to redux store.
+
+const interceptor = axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const { status, data } = error.response;
+
+    if (status === UNAUTHORIZED) {
+      if (data.error === 'token expired') {
+        dispatch(logout('login'));
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default interceptor;
