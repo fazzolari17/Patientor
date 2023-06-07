55001479ece578241b199daaf6a6f57ed49a2cd5
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-06-07 15:22:19 -0400

    update url for production

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,6 +1,6 @@
 const PORT = process.env.port || 3001;
 // const productionPORT = process.env.PORT || 10000;
-const production = `https://patientor-ccvj.onrender.com`;
+const production = `https://patientor-server-docker.onrender.com`;
 const local = `http://localhost:${PORT}`;
 
 const url = process.env.NODE_ENV === 'production' ? production : local;
